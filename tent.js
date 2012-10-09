85a8dcaa659b02949ba0c8629439b94974d947c9
Sebastian Mai <bastinat0r@bastinat0r.de>
2012-10-09 23:06:00 +0200

    doing many things rigth, oh so many things …

--- tent.js
+++ tent.js
@@ -1,26 +1,29 @@
 var https = require('https');
 var util = require('util');
+var url = require('url');
 
+exports.registerApp = registerApp;
 
-function sendAuthenticationRequest(opts, appInfo) {
-	var reqOpts = opts;
-	reqOpts.path = '/apps';
-	reqOpts.method = 'POST';
-	reqOpts.headers = {
-		'Content-Type' : 'application/vnd.tent.v0+json',
-		'Accept' : 'application/vnd.tent.v0+json',
-		'Content-Length' : JSON.stringify(appInfo).length
-	}
-	
-	var req = https.request(reqOpts, function(res) {
-		res.on('data', function(data) {
-			util.puts(data);
-		});
-		res.on('end', function() {
-			util.puts(JSON.stringify(res.headers));
-		});
-	});
-	req.end(JSON.stringify(appInfo));
+var debug = true;
+
+function printErrorMessage(error) {
+	util.puts('ERROR: \n' + error);
+}
+
+function printBody(data) {
+	if(debug)	util.puts('Response Body:\n'+data);
+}
+
+function printHeader(headers) {
+	if(debug)	util.puts('HTTP Headers:\n'+JSON.stringify(headers));
+}
+
+function printOptions(opts) {
+	if(debug)	util.puts('HTTP Options:\n'+JSON.stringify(opts));
+}
+
+function printUrl(url) {
+	if(debug)	util.puts('URL:\n'+url);
 }
 
 function registerApp(credentials, appInfo) {
@@ -33,14 +36,48 @@ function registerApp(credentials, appInfo) {
 			"Content-Length" : "0",
 		}
 	}
-
 	var req = https.request(opts, function(res) {
 		res.on('end', function() {
-			util.puts(JSON.stringify(res.headers));
-			sendAuthenticationRequest(opts, appInfo);
+			printHeader(res.headers);
+			
+			// the link looks like this: <https:// ... >, we want it
+			var profileUrl = /<[^>]*>/.exec(res.headers.link);
+			// now we want to get rid of the '<' and '>'
+			profileUrl = (""+profileUrl).replace(/[<>]/g,'');
+			printUrl(profileUrl);
+			https.get(url.parse(profileUrl), function(res) {
+				res.on('data', function(data) {
+					printBody(data);
+					var profile = JSON.parse(data);
+					var core = profile["https://tent.io/types/info/core/v0.1.0"];
+					sendAuthenticationRequest(core, appInfo);
+				});
+			});
 		});
 	});
+	req.on('error', printErrorMessage);
 	req.end();
 }
 
-exports.registerApp = registerApp;
+function sendAuthenticationRequest(profileCore, appInfo) {
+	var contentLength = JSON.stringify(appInfo).length;
+	var apiRootUrl = profileCore.servers[0];
+	var opts = url.parse(apiRootUrl);
+	opts.method = 'POST';
+	opts.path = opts.path + '/apps';
+	opts.headers = {
+		"Accept" : "application/vnd.tent.v0+json",
+		"Content-Type" : "application/vnd.tent.v0+json",
+		"Content-Length" : contentLength.toString()
+	}
+	printOptions(opts);
+	var req = https.request(opts, function(res) {
+		res.on('data', printBody);
+		res.on('end', function() {
+			printHeader(res.headers);
+		});
+	});
+	req.on('error', printErrorMessage);
+	req.end(JSON.stringify(appInfo));
+}
+
